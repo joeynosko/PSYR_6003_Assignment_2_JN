c57eeed6d819de1014e579d02be1a3b74166d3d5
Joey Nosko <joey.nosko@gmail.com>
2025-03-14 19:50:44 -0300

    Initial commit

---
+++ assignment_2.R
@@ -0,0 +1,33 @@
+library(haven)
+library(tidyverse)
+library(flexplot)
+PSYR6003_A2 <- read_sav("PSYR6003.A2.sav")#load the data.
+View(PSYR6003_A2)
+
+#Data Cleaning
+
+PSYR6003_A2_noNA <- na.omit(PSYR6003_A2)#remove cases with missing data
+view(PSYR6003_A2_noNA)
+
+A2_Clean <- PSYR6003_A2_noNA %>%
+  filter(sex != "Other (specify)") #remove case with "other"
+view(A2_Clean)
+
+A2_Clean_MF <- A2_Clean %>% mutate(sex = ifelse(sex == "Male", "1", "0")) %>% 
+  mutate(sex = as.factor(sex))#change sex variable to be factor 
+view(A2_Clean_MF)
+
+#Reverse coding tipm.CONS2.3y
+reverse_cols = c("tipm.CONS2.3y")#create vector with our variable of interest
+A2_Clean_MF[, reverse_cols] = 8 - A2_Clean_MF[, reverse_cols]#subtract 8 from each value in the column. 
+view(A2_Clean_MF_ST)#check to see if I messed up the data
+
+#Creating a new variable representing the mean scores for each scale. 
+A2 <- A2_Clean_MF %>%
+  mutate(SPP_mean = rowMeans(select(., mps.SPP1.3y, mps.SPP2.3y, mps.SPP3.3y, mps.SPP4.3y, mps.SPP5.3y))) %>%
+  mutate(NAf_mean = rowMeans(select(., guilt1.3y, guilt2.3y, guilt3.3y, dep1.3y, dep2.3y, dep3.3y, fear1.3y, fear2.3y, fear3.3y, host1.3y, host2.3y, host3.3y))) %>%
+  mutate(CON_mean = rowMeans(select(., tipm.CONS1.3y, tipm.CONS2.3y)))
+view(A2)
+
+#negative affect = b0 + b1*SPP + b2*NAf + b3*sex + e
+#negative affect = b0 + b2*NAf + b3*sex + e
